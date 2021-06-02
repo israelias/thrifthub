02c3c78af433b514d1766b68d840c1eb9e1715ef
israelias <israelias.js@gmail.com>
2021-06-02 17:57:55 +0800

    Add rest_framework to core apps

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "store",
     "mptt",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,9 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+}
