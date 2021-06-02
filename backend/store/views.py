833813aa9edc09b68d3d1deef87e5d00f02b52c1
israelias <israelias.js@gmail.com>
2021-06-02 18:02:32 +0800

    Connect model and serializer for api views
    Product list, product item, category and category list are initiated as the views for frontend to receive as json data.

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class Product(generics.RetrieveAPIView):
+    lookup_field = "slug"
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryItemView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return models.Product.objects.filter(
+            category__in=Category.objects.get(slug=self.kwargs["slug"]).get_descendants(include_self=True)
+        )
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.filter(level=1)
+    serializer_class = CategorySerializer
