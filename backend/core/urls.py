5f9045407eb145b2b8c1d4aaba4883c311d817e5
israelias <israelias.js@gmail.com>
2021-06-02 17:57:04 +0800

    Add inclusions for store.urls in core.urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("store.urls", namespace="store")),
+    path("", include("store.urls", namespace="store")),
 ]
 
 if settings.DEBUG:
