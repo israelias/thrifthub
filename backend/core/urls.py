a39af7bec89170e9dd7472ff971296edda283991
israelias <israelias.js@gmail.com>
2021-06-12 18:18:28 +0800

    Add graphene views and graphql extensions

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,9 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+from graphql_jwt.decorators import jwt_cookie
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Cookies automatically save and no need to add to authorization request
+    path("graphql/", jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
     path("", include("store.urls", namespace="store")),
 ]
 
