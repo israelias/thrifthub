066a780c6e88cb989629f16efa2c9e3243c6dbba
israelias <israelias.js@gmail.com>
2021-06-12 18:17:01 +0800

    Initialize account app with graphql schema

---
+++ backend/account/schema.py
@@ -0,0 +1,30 @@
+import graphene
+import graphql_jwt
+from django.contrib.auth.models import User
+from graphene_django import DjangoObjectType
+from graphql_jwt.decorators import login_required
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email")
+
+
+class Query(graphene.ObjectType):
+    user_details = graphene.Field(UserType)
+
+    def resolve_user_details(self, info, **kwargs):
+        user = info.context.user
+        if not user.is_authenticated:
+            raise Exception("Authentication credentials were not provided")
+        return User.objects.get(username=user)
+
+
+class Mutation(graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+
