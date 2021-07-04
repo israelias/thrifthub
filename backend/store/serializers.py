c8fc7dbda0ca1038547fa5c44285148120f79a78
israelias <israelias.js@gmail.com>
2021-07-04 19:21:39 +0800

    Add cart serializers to store products

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,7 +1,16 @@
+import random
+
+
+# import order.models as order_models
+# import vendor.models
+# import vendor.serializers as vendor_serializers
+# from order import serializers as order_serializers
 from rest_framework import serializers
-from vendor.serializers import VendorSerializer
 
 from .models import Category, Product, ProductImage
+
+# from vendor.serializers import VendorAdminSerializer
+# from vendor import serializers as vendor_serializers
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -20,9 +29,48 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
-    vendor = VendorSerializer(read_only=True)
+    # vendor = vendor_serializers.VendorSerializer(read_only=True)
     product_image = ImageSerializer(many=True, read_only=True)
+
+    vendor = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Product
         fields = ["id", "category", "vendor", "title", "description", "slug", "regular_price", "product_image"]
+
+
+class ProductCartSerializer(serializers.BaseSerializer):
+    category = CategorySerializer(read_only=True)
+    # vendor = vendor_serializers.VendorSerializer(read_only=True)
+    product_image = ImageSerializer(many=True, read_only=True)
+    vendor = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ["id", "category", "vendor", "title", "description", "slug", "regular_price", "product_image"]
+
+    def to_representation(self, instance):
+        imagesstring = '{"thumbnail": "%s", "image": "%s", "id": "mainimage"},' % (
+            instance.get_thumbnail(),
+            instance.image.url,
+        )
+
+        for image in instance.images.all():
+            imagesstring += '{"thumbnail": "%s", "image": "%s", "id": "%s"},' % (
+                image.get_thumbnail(),
+                image.image.url,
+                image.id,
+            )
+
+        print(imagesstring)
+
+        similar_products = list(instance.category.products.exclude(id=instance.id))
+
+        if len(similar_products) >= 4:
+            similar_products = random.sample(similar_products, 4)
+
+        return {
+            "product": instance,
+            "similar_products": similar_products,
+            "imagesstring": "[" + imagesstring.rstrip(",") + "]",
+        }
