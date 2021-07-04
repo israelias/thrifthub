9693ea32b45a280194a8e277d274266c4ef8899b
israelias <israelias.js@gmail.com>
2021-07-04 19:23:38 +0800

    Add product inlines to vendor admin views

--- backend/vendor/admin.py
+++ backend/vendor/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
+from order.admin import OrderItemInline
+from store.models import Product
 
 from .models import Vendor
 
-admin.site.register(Vendor)
+# admin.site.register(Vendor)
 # class VendorAdmin(admin.ModelAdmin):
 #     model = Vendor
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+
+
+@admin.register(Vendor)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        # ProductCategoryInline,
+        # ProductVendorInline,
+        # ProductImageInline,
+        ProductInline,
+        OrderItemInline,
+    ]
