fa4af844d8437f8e46cf71fdd1091dda44759009
israelias <israelias.js@gmail.com>
2021-07-08 01:34:19 +0800

    Separate swagger/admin/redoc UI from data endpoints

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,20 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    # Cookies automatically save and no need to add to authorization request
-    path("graphql/", jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
+from .yasg import schema_swagger_view, schema_view
+
+# ---------- Django REST ----------#
+
+urlpatterns_root = [
     path("", include("store.urls", namespace="store")),
     path("", include("vendor.urls", namespace="vendor")),
+    path("", include("order.urls", namespace="order")),
 ]
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("graphql/", jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
+    url(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    url(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    url(r"^api/", include(urlpatterns_root)),
+]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
