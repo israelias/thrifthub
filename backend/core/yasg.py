bd6e219ab3822aa3a6649246527277ea7697039d
israelias <israelias.js@gmail.com>
2021-07-08 01:35:14 +0800

    Write custom schema and tag generator for API docs

---
+++ backend/core/yasg.py
@@ -0,0 +1,39 @@
+from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+from drf_yasg.inspectors import SwaggerAutoSchema
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_swagger.views import get_swagger_view
+
+
+# https://github.com/axnsan12/drf-yasg/issues/146
+class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, *args, **kwargs):
+        schema = super().get_schema(*args, **kwargs)
+        schema.basePath = "/api/v1.0"  # API prefix
+        return schema
+
+
+class CompoundTagsSchema(SwaggerAutoSchema):
+
+    # See https://github.com/axnsan12/drf-yasg/issues/56
+    def get_tags(self, operation_keys):
+        return [" > ".join(operation_keys[:-1])]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ThriftHub API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="israelias.js@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    generator_class=CustomOpenAPISchemaGenerator,
+   
+)
+
+schema_swagger_view = get_swagger_view(title="ThriftHub API")
