e9523edb557bf3fb050a13522b054518b4b3dbd3
israelias <israelias.js@gmail.com>
2021-07-08 01:31:57 +0800

    Add ver-image, dj-filters, drf_yasg to app extensions

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,26 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STRIPE_PUB_KEY = (
+    "pk_test_51J2BHrIPDTNVSO6kqBTaTI8e0kXrWpRbZa89Hb7M5ydv5onBDdondvujfY5xzOjSRr9EUobHYpeUt2u7krvjm3hS00yzAJCyf8"
+)
+STRIPE_SECRET_KEY = (
+    "sk_test_51J2BHrIPDTNVSO6kKo7Brsue8UK0jGsN32tIaTMPeGhoL2x2kO571NSH56SbvFNfFNZKUXcFT3beJfCPCXMAvpB6005lF8gN3M"
+)
+
+# LOGIN_URL = "login"
+# LOGIN_REDIRECT_URL = "vendor_admin"
+# LOGOUT_REDIRECT_URL = "frontpage"
+
+SESSION_COOKIE_AGE = 86400
+CART_SESSION_ID = "cart"
+
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = "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"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_EMAIL_FROM = "Interiorstore <noreply@codewithstein.com>"
 
 # Application definition
 
@@ -40,9 +60,14 @@ INSTALLED_APPS = [
     "store",
     "account",
     "vendor",
+    "order",
     "corsheaders",
     "mptt",
+    "versatileimagefield",
+    "django_filters",
     "rest_framework",
+    "drf_yasg",
+    # "rest_framework_swagger",
     "graphene_django",
 ]
 
@@ -77,6 +102,54 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+SWAGGER_SETTINGS = {
+    "DEFAULT_AUTO_SCHEMA_CLASS": "core.yasg.CompoundTagsSchema",
+    "DEFAULT_GENERATOR_CLASS": "core.yasg.CustomOpenAPISchemaGenerator",
+    # "DEFAULT_API_URL":
+}
+
+VERSATILEIMAGEFIELD_SETTINGS = {
+    # The amount of time, in seconds, that references to created images
+    # should be stored in the cache. Defaults to `2592000` (30 days)
+    "cache_length": 2592000,
+    # The name of the cache you'd like `django-versatileimagefield` to use.
+    # Defaults to 'versatileimagefield_cache'. If no cache exists with the name
+    # provided, the 'default' cache will be used instead.
+    "cache_name": "versatileimagefield_cache",
+    # The save quality of modified JPEG images. More info here:
+    # https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#jpeg
+    # Defaults to 70
+    "jpeg_resize_quality": 70,
+    # The name of the top-level folder within storage classes to save all
+    # sized images. Defaults to '__sized__'
+    "sized_directory_name": "__sized__",
+    # The name of the directory to save all filtered images within.
+    # Defaults to '__filtered__':
+    "filtered_directory_name": "__filtered__",
+    # The name of the directory to save placeholder images within.
+    # Defaults to '__placeholder__':
+    "placeholder_directory_name": "__placeholder__",
+    # Whether or not to create new images on-the-fly. Set this to `False` for
+    # speedy performance but don't forget to 'pre-warm' to ensure they're
+    # created and available at the appropriate URL.
+    "create_images_on_demand": True,
+    # A dot-notated python path string to a function that processes sized
+    # image keys. Typically used to md5-ify the 'image key' portion of the
+    # filename, giving each a uniform length.
+    # `django-versatileimagefield` ships with two post processors:
+    # 1. 'versatileimagefield.processors.md5' Returns a full length (32 char)
+    #    md5 hash of `image_key`.
+    # 2. 'versatileimagefield.processors.md5_16' Returns the first 16 chars
+    #    of the 32 character md5 hash of `image_key`.
+    # By default, image_keys are unprocessed. To write your own processor,
+    # just define a function (that can be imported from your project's
+    # python path) that takes a single argument, `image_key` and returns
+    # a string.
+    "image_key_post_processor": None,
+    # Whether to create progressive JPEGs. Read more about progressive JPEGs
+    # here: https://optimus.io/support/progressive-jpeg/
+    "progressive_jpeg": False,
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -138,7 +211,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"]
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"],
+    # The django-filter library includes a DjangoFilterBackend class
+    # which supports highly customizable field filtering for REST framework.
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+    "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.URLPathVersioning",
+    "VALIDATOR_URL": "http://localhost:8000",
 }
 
 GRAPHENE = {
