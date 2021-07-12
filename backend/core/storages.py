6afd17af8b4835037240af6738c1a972fb3edb81
israelias <israelias.js@gmail.com>
2021-07-13 02:47:31 +0800

    Add bucket name to storage classes

--- backend/core/storages.py
+++ backend/core/storages.py
@@ -3,8 +3,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
     location = settings.MEDIAFILES_LOCATION
