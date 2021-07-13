61c56b0cab73cc1fe74d0ac9d6246c30fbcfd77b
israelias <israelias.js@gmail.com>
2021-07-13 18:52:35 +0800

    Revert static dir settings to tuple format

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -149,8 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATICFILES_DIRS = str(BASE_DIR / "static/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = str(BASE_DIR / "static/")
 # STATICFILES_DIRS = [
 #     str(BASE_DIR / "static"),
 #     "/var/www/static/",
