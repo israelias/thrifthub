15a198d31848d45a3b6df03b9a9a736ae5ece63f
israelias <israelias.js@gmail.com>
2021-07-18 04:20:41 +0800

    Write string utils to initialize user/vendor names

---
+++ frontend-web/utils/initialize.ts
@@ -0,0 +1,7 @@
+export const initialize = (name: string) => {
+  return name.charAt(0).toUpperCase() + name.charAt(1).toUpperCase();
+};
+
+export const firstInitial = (name: string) => {
+  return name.charAt(0).toUpperCase();
+};
