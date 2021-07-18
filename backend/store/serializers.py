b2bfaf48b0a696ac8e6d19ee8509337ea7f7c43b
israelias <israelias.js@gmail.com>
2021-07-18 23:15:37 +0800

    Add get_display method to condition serializer

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -58,6 +58,7 @@ class ImageNewSerializer(FlexFieldsModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     vendor = VendorSlugSerializer(read_only=True)
+    condition = serializers.CharField(source="get_condition_display")
     product_images = ImageNewSerializer(many=True, read_only=True)
 
     class Meta:
@@ -71,32 +72,17 @@ class ProductSerializer(serializers.ModelSerializer):
             "slug",
             "price",
             "is_available",
+            "condition",
+            "created_at",
+            "updated_at",
             "product_images",
         ]
 
 
 class ProductPreviewSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
-    vendor = VendorSlugSerializer(read_only=True)
-    product_images = ImageNewSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Product
-        fields = [
-            "id",
-            "category",
-            "vendor",
-            "title",
-            "slug",
-            "price",
-            "is_available",
-            "product_images",
-        ]
-
-
-class ProductDetailSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField(read_only=True)
     vendor = serializers.StringRelatedField(read_only=True)
+    condition = serializers.CharField(source="get_condition_display")
     product_images = ImageNewSerializer(many=True, read_only=True)
 
     class Meta:
@@ -106,12 +92,15 @@ class ProductDetailSerializer(serializers.ModelSerializer):
             "category",
             "vendor",
             "title",
-            "description",
             "slug",
             "price",
+            "condition",
             "is_available",
+            "created_at",
+            "updated_at",
             "product_images",
         ]
+
 
 
 class VendorFullSerializer(serializers.ModelSerializer):
@@ -127,6 +116,7 @@ class CategoryFullSerializer(serializers.ModelSerializer):
 
 
 class ProductSimilarSerializer(FlexFieldsModelSerializer):
+    condition = serializers.CharField(source="get_condition_display")
     class Meta:
         model = Product
         fields = [
@@ -138,6 +128,7 @@ class ProductSimilarSerializer(FlexFieldsModelSerializer):
             "slug",
             "price",
             "is_available",
+            "condition",
             "product_images",
         ]
         expandable_fields = {
@@ -149,6 +140,7 @@ class ProductSimilarSerializer(FlexFieldsModelSerializer):
 
 class ProductVersatileSerializer(FlexFieldsModelSerializer):
     similar_products = serializers.SerializerMethodField()
+    condition = serializers.CharField(source="get_condition_display")
     absolute_url = serializers.CharField(source="get_absolute_url", read_only=True)
 
     class Meta:
@@ -161,10 +153,13 @@ class ProductVersatileSerializer(FlexFieldsModelSerializer):
             "description",
             "slug",
             "price",
+            "condition",
             "is_available",
             "product_images",
             "similar_products",
             "absolute_url",
+            "created_at",
+            "updated_at",
         ]
         expandable_fields = {
             "category": CategoryFullSerializer,
