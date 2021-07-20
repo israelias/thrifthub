de418aa19c67c0e052c3649e4da81ff34a1cd268
israelias <israelias.js@gmail.com>
2021-07-20 21:35:58 +0800

    Add test rout param types

--- frontend-web/types.tsx
+++ frontend-web/types.tsx
@@ -3,105 +3,8 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-export type User = {
-  username: string;
-  first_name?: string;
-  last_name?: string;
-  email?: string;
-  password?: string;
-  is_active?: boolean;
-  last_login?: string;
-  date_joined?: string;
-};
-
-export type Vendor = {
-  id: number;
-  name: string;
-  slug: string;
-  created_at: string;
-  created_by: User;
-  products: Product[];
-  favorites: Product[];
-  friends: Vendor[];
-  friends_products: Product[];
-  order_requests: Order[];
-  orders_made: Order[];
-  online?: boolean;
-};
-
-export type Category = {
-  id: number;
-  name: string;
-  slug: string;
-  lft: number;
-  rght: number;
-  tree_id: number;
-  level: number;
-  parent_id: number;
-};
-
-export type Product = {
-  id: number;
-  category: Category["id"];
-  vendor: Vendor["id"];
-  title: string;
-  description: string;
-  slug: string;
-  price: number;
-  is_available: boolean;
-  created_at?: string;
-  updated_at?: string;
-  product_images?: Image[];
-  similiar_products?: Product[];
-};
-
-export type Image = {
-  id?: number;
-  name?: string;
-  image: {
-    full_size: string;
-    thumbnail: string;
-  };
-  alt_text: string;
-  is_feature: boolean;
-  created_at?: string;
-  updated_at?: string;
-  product?: Product["id"];
-};
-
-export type Order = {
-  id: number;
-  product: Product["id"];
-  vendor: Vendor["id"];
-  buyer: Vendor["id"];
-  status: string;
-  amount?: string;
-  created_at?: string;
-  order_detail?: OrderDetail;
-};
-
-export type OrderDetail = {
-  id: number;
-  full_name: string;
-  email: string;
-  phone_number: string;
-  country: string;
-  zipcode?: string;
-  town_or_city: string;
-  street_address1: string;
-  street_address2?: string;
-  county?: string;
-  created_at?: string;
-  updated_at?: string;
-  stripe_pid?: string;
-  order?: Order["id"];
-};
-
-export type AuthResponse = {
-  access: string;
-  refresh: string;
-  user: Vendor;
-};
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RouteProp } from "@react-navigation/native";
 
 //  full_name = models.CharField(max_length=50, null=False, blank=False)
 //     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -144,3 +47,91 @@ export type TabOneParamList = {
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
 };
+
+export type RootParamList = {
+  LandingScreen: undefined;
+  StoreScreen: undefined;
+  FavoriteScreen: undefined;
+  OrderScreen: { paramA: string };
+  SellScreen: { paramB: string; paramC: number };
+  BuyScreen: undefined;
+};
+
+export type ProductParamList = {
+  Product: {
+    name: string;
+  };
+  EditProduct: {
+    name: string;
+    submit?: React.MutableRefObject<() => void>;
+  };
+};
+
+export type SearchParamList = {
+  Search: undefined;
+} & ProductParamList;
+
+export type SearchStackNavProps<T extends keyof SearchParamList> = {
+  navigation: StackNavigationProp<SearchParamList, T>;
+  route: RouteProp<SearchParamList, T>;
+};
+
+export type AuthParamList = {
+  Login: undefined;
+  Register: undefined;
+};
+
+export type AuthNavProps<T extends keyof AuthParamList> = {
+  navigation: StackNavigationProp<AuthParamList, T>;
+  route: RouteProp<AuthParamList, T>;
+};
+
+export type HomeParamList = {
+  Feed: undefined;
+} & ProductParamList;
+
+export type HomeStackNavProps<T extends keyof HomeParamList> = {
+  navigation: StackNavigationProp<HomeParamList, T>;
+  route: RouteProp<HomeParamList, T>;
+};
+
+// creating a screen
+
+// const Screen1 = ({ navigation, route }: Screen1Props) => {
+//   return (
+//     <>
+//       <Text>Screen1</Text>
+//       <Button
+//         onPress={() => {
+//           navigation.push("Screen2", { paramA: "Hello!" });
+//         }}
+//       />
+//     </>
+//   );
+// };
+
+// Accessing params from the next screen
+
+// const Screen2 = ({ route }: Screen2Props) => {
+//   return <Text>{route.params.paramA}</Text>;
+// };
+
+// Or
+
+// using hooks to access the route params
+
+// const Screen1 = () => {
+//   const navigation =
+//     useNavigation<StackNavigationProp<RootParamList, "Screen1">>();
+//   const route = useRoute<RouteProp<RootParamList, "Screen1">>();
+
+//   // ...
+// };
+
+// <NavigationContainer>
+//   <Root.Navigator>
+//     <Root.Screen name="Screen1" component={Screen1} />
+//     <Root.Screen name="Screen2" component={Screen2} />
+//     <Root.Screen name="Screen3" component={Screen3} />
+//   </Root.Navigator>
+// </NavigationContainer>;
