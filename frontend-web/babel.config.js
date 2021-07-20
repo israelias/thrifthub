870b68f03227b94615c20d4ac9f0e746a33a8b88
israelias <israelias.js@gmail.com>
2021-07-20 21:38:08 +0800

    Add dev and prod plugins to babel config

--- frontend-web/babel.config.js
+++ frontend-web/babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    env: {
+      development: {
+        plugins: ["transform-react-jsx-source"],
+      },
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
