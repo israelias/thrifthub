cdb5f76a521e74223fa99c54a9a80dd4c1508ac5
israelias <israelias.js@gmail.com>
2021-07-20 13:59:18 +0800

    Update orderdetail views and endpoints to a viewset

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -7,9 +7,10 @@ app_name = "order"
 
 router = DefaultRouter()
 router.register(r"orders", views.VendorOrderViewSet, basename="vendor_order")
+router.register(r"orderdetail", views.VendorOrderDetailViewSet, basename="vendor_order_detail")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("orderitems/", views.OrderItemListView.as_view(), name="orderitems"),
-    path("orderitems/<slug:id>/", views.OrderItem.as_view(), name="orderitem"),
+    # path("orderitems/", views.OrderItemListView.as_view(), name="orderitems"),
+    # path("orderitems/<slug:id>/", views.OrderItem.as_view(), name="orderitem"),
 ]
--- backend/order/views.py
+++ backend/order/views.py
@@ -18,19 +18,17 @@ class OrderListView(generics.ListAPIView):
 
 class VendorOrderViewSet(FlexFieldsModelViewSet):
     """
-    Vendo Order View Set.
+    Vendor Order View Set.
     With Flex-Expandable fields.
     With search fields.
-
-    A viewset that provides default `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
 
 
     Flex Endpoint Options: `api/order/?expand=vendor,buyer,product`
     @see https://github.com/rsinger86/drf-flex-fieldshttps://github.com/rsinger86/drf-flex-fields
 
     Search Endpoint: `api/order/?search=<query>`
-    Sample Request:
+
+    Sample Request To Purchase or Make an Offer as a buyer:
     {
         "product": "1",
         "buyer": "4",
@@ -39,12 +37,43 @@ class VendorOrderViewSet(FlexFieldsModelViewSet):
         "refresh": <REFRESH TOKEN>
     }
 
+    Status immediately updates to PROCESSING if amount is equal to product price.
+
+    Sample Request To Accept or Deny an offer
+    {
+        "product": "1",
+        "buyer": "4",
+        "status": "ACCEPTED",
+
+        "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjI3OTc4MTYxLCJqdGkiOiI1ZGFiZjMzMmYwMzM0ZWQyOTliYzEzODdmMDRjODRiNSIsInVzZXJfaWQiOjF9.W-mkEE6A0YsznE2d1gSIUKViwDWvmH1-HiMm-YzwJltGfwQSNVZ9vQMfmzQEzyNYYzL6z3a7rVB8NWi7IEtP0A",
+        "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTYyNzk3ODE2MSwianRpIjoiMWUyNTY3OWVkMDlmNDM5NWJlODRkMjFlMDM4NWM3MTAiLCJ1c2VyX2lkIjoxfQ.DPduTSVmZwmb2lg_00d9mrhQ9NO3exMMPrqmm49hW0liuPQLqruLmGChkZXk4W03d_UwBl714ZltRX8yFZxugw"
+    }
+
+
+
     """
 
     permit_list_expands = ["vendor", "product", "buyer"]
     lookup_field = "id"
     queryset = OrderModel.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (AllowAny,)
+
+
+class VendorOrderDetailViewSet(FlexFieldsModelViewSet):
+    """
+    Vendor Order Detail View Set.
+    With Flex-Expandable fields.
+
+
+    List Endpont: `api/orderitem/`
+    Detail Endpont: `api/orderitem/<id>`
+
+    """
+
+    lookup_field = "id"
+    queryset = OrderDetail.objects.all()
+    serializer_class = OrderDetailSerializer
     permission_classes = (AllowAny,)
 
 
