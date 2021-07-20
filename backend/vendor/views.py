c24c6ae32d27b88fb87b74c68043f895121b626b
israelias <israelias.js@gmail.com>
2021-07-20 14:08:04 +0800

    Remove obsolete views

--- backend/vendor/views.py
+++ backend/vendor/views.py
@@ -28,11 +28,9 @@ from .models import Friend, Vendor
 from .serializers import (
     CurrentVendorSerializer,
     OtherVendorSerializer,
-    VendorAdminSerializer,
     VendorFavoritesSerializer,
     VendorFriendSerializer,
     VendorProductSerializer,
-    VendorSearchSerializer,
     VendorSerializer,
 )
 
@@ -87,11 +85,6 @@ class OtherVendorDetailView(generics.RetrieveUpdateAPIView):
     queryset = Vendor.objects.all()
     serializer_class = OtherVendorSerializer
     lookup_field = "id"
-
-
-class VendorListView(generics.ListAPIView):
-    queryset = Vendor.objects.all()
-    serializer_class = CurrentVendorSerializer
 
 
 class VendorFavoriteListView(generics.ListCreateAPIView):
@@ -168,105 +161,3 @@ class VendorFriendsListView(generics.ListCreateAPIView):
             data={"message": "Removed from Friends"},
             status=status.HTTP_200_OK,
         )
-
-
-class VendorSearchListView(generics.ListAPIView):
-    """
-    Endpoint for searching other vendors by name.
-    """
-
-    serializer_class = VendorSearchSerializer
-
-    def get_queryset(self):
-        query = self.kwargs["query"]
-        queryset = Vendor.objects.filter(name__contains=query)
-        return queryset
-
-
-# class VendorList(generics.ListCreateAPIView):
-#     """
-#     All Vendors
-#     This API view supports post and get requests.
-#     """
-
-#     queryset = Vendor.objects.all()
-#     serializer_class = VendorSerializer
-
-#     """
-#     Endpoint to become a vendor.
-#     """
-
-#     def perform_create(self, serializer):
-#         serializer.save(name=self.request.user.username, created_by=self.request.user)
-
-
-class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update, delete a vendor.
-    """
-
-    queryset = Vendor.objects.all()
-    serializer_class = VendorSerializer
-
-
-class VendorProductListView(generics.ListCreateAPIView):
-    """
-    Provides get and post method handlers
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(vendor=self.request.user)
-
-
-class VendorProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Provides get, put, patch and delete method handlers
-    """
-
-    lookup_field = "slug"
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
-
-
-class VendorProductCreateApi(generics.CreateAPIView):
-    """
-    Create a product as a vendor.
-    This API view supports only post requests.
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
-
-
-class VendorProductApi(generics.ListAPIView):
-    """
-    Get a list of all products.
-    This API view supports only get requests.
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
-
-
-class VendorProductUpdateApi(generics.RetrieveUpdateAPIView):
-    """
-    Updating can be performed simply by replacing
-    the ListAPIView with RetriveUpdateAPIView.
-    This API view supports both get, put, and patch requests.
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
-
-
-class VendorProductDeleteApi(generics.DestroyAPIView):
-    """
-    Delete a Vendor.
-    This API view provides only delete requests
-    """
-
-    queryset = Product.objects.all()
-    serializer_class = VendorProductSerializer
