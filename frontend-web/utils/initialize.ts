48125f13dccce141d3d8b387873b9f846a852423
israelias <israelias.js@gmail.com>
2021-07-23 20:58:28 +0800

    Move entire root ui to src folder

--- frontend-web/utils/initialize.ts
+++
@@ -1,7 +0,0 @@
-export const initialize = (name: string) => {
-  return name.charAt(0).toUpperCase() + name.charAt(1).toUpperCase();
-};
-
-export const firstInitial = (name: string) => {
-  return name.charAt(0).toUpperCase();
-};
