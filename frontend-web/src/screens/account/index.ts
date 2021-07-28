6a8a37ef2306b0eed661c061fc416c7f6b8a0b66
israelias <israelias.js@gmail.com>
2021-07-29 04:11:58 +0800

    Remove previous frontend-web directory [ci skip]

--- frontend-web/src/screens/account/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as StartScreen } from "./startScreen";
-export { default as LoginScreen } from "./loginScreen";
-export { default as RegisterScreen } from "./registerScreen";
-export { default as ResetPasswordScreen } from "./resetPasswordScreen";
-export { default as Dashboard } from "./dashboardScreen";
