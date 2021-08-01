cf3e1f8d05650491fb25f1b826b59e03f9f5974a
israelias <israelias.js@gmail.com>
2021-08-02 03:18:14 +0800

    Add myProducts screen as child of main stack navigator

---
+++ frontend/src/screens/vendor/myProductsScreen.tsx
@@ -0,0 +1,54 @@
+import React, { ComponentType } from 'react';
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useTheme, ActivityIndicator } from 'react-native-paper';
+import {
+  ProductStackNavigatorParamList,
+  ProductTypeParamList,
+} from '../../types';
+import { useVendorData } from '../../context/vendor.context';
+import { Product } from '../../components/products/product';
+
+type ProductProps = ProductStackNavigatorParamList['ProductDetails'];
+
+function Separator() {
+  return <View style={{ height: StyleSheet.hairlineWidth }} />;
+}
+
+type Props = {
+  navigation?: StackNavigationProp<ProductStackNavigatorParamList>;
+};
+
+export const MyProductsScreen = (props: Props) => {
+  const theme = useTheme();
+  const { vendor, error, loading } = useVendorData();
+
+  return loading ? (
+    <ActivityIndicator />
+  ) : vendor && vendor.products.length < 1 ? (
+    <Text>You Haven't saved posted any products yet!</Text>
+  ) : (
+    <FlatList
+      contentContainerStyle={{
+        backgroundColor: theme.colors.background,
+      }}
+      style={{ backgroundColor: theme.colors.background }}
+      data={
+        vendor &&
+        vendor.products.map((productProps) => ({
+          ...productProps,
+          onPress: () =>
+            props.navigation &&
+            props.navigation.push('ProductDetails', {
+              ...productProps,
+            }),
+        }))
+      }
+      renderItem={({ item }: { item: ProductProps }) => (
+        <Product product={item} />
+      )}
+      keyExtractor={(item: ProductProps) => item.id.toString()}
+      ItemSeparatorComponent={Separator}
+    />
+  );
+};
