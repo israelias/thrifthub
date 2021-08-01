fec81faf8032804d091fffbf35539502446a113b
israelias <israelias.js@gmail.com>
2021-08-02 03:14:42 +0800

    Add offer/purchase actions to product detail screen

--- frontend/src/screens/products/productDetailScreen.tsx
+++ frontend/src/screens/products/productDetailScreen.tsx
@@ -1,12 +1,33 @@
-import React from "react";
-import { RouteProp } from "@react-navigation/native";
-import { ProductStackNavigatorParamList } from "../../types";
-import { ProductDetail } from "../../components/products/productDetail";
+import React from 'react';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { ProductStackNavigatorParamList } from '../../types';
+import { ProductDetail } from '../../components/products/productDetail';
 
-type Props = {
-  route: RouteProp<ProductStackNavigatorParamList, "ProductDetails">;
-};
+type ProductProps = ProductStackNavigatorParamList['ProductDetails'];
 
-export const ProductDetailScreen = (props: Props) => {
-  return <ProductDetail {...props.route.params} />;
+export const ProductDetailScreen = ({
+  route,
+  navigation,
+}: {
+  route: RouteProp<ProductStackNavigatorParamList, 'ProductDetails'>;
+  navigation?: StackNavigationProp<ProductStackNavigatorParamList>;
+}) => {
+  const makeOffer = (product: ProductProps) =>
+    navigation &&
+    navigation.navigate('MakeOffer', {
+      product,
+      productId: product.id.toString(),
+      vendorId: product.vendor.id.toString(),
+      ...product,
+    });
+  console.log('is this whats making it work');
+  return (
+    <ProductDetail
+      {...route.params}
+      product={route.params}
+      navigation={navigation}
+      makeOffer={() => makeOffer}
+    />
+  );
 };
