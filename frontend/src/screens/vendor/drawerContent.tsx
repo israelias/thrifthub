0bfa3b05dc67a4b0cb4654456d52c779c517454f
israelias <israelias.js@gmail.com>
2021-08-02 03:17:35 +0800

    Update drawer content to use constants and custom nav hooks

--- frontend/src/screens/vendor/drawerContent.tsx
+++ frontend/src/screens/vendor/drawerContent.tsx
@@ -1,13 +1,24 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import {
   DrawerContentComponentProps,
   DrawerContentOptions,
   DrawerNavigationProp,
   DrawerContentScrollView,
   DrawerItem,
-} from "@react-navigation/drawer";
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+} from '@react-navigation/drawer';
+import {
+  getFocusedRouteNameFromRoute,
+  DrawerActions,
+  StackActions,
+  ParamListBase,
+} from '@react-navigation/native';
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  ScrollViewProps,
+  View,
+} from 'react-native';
 import {
   Avatar,
   Caption,
@@ -18,27 +29,121 @@ import {
   Title,
   TouchableRipple,
   useTheme,
-} from "react-native-paper";
-import Animated from "react-native-reanimated";
+  Button,
+} from 'react-native-paper';
+import Animated from 'react-native-reanimated';
+import * as ICONS from '../../constants/icons.constants';
+import { usePreference } from '../../context/preferences.context';
 
-import { PreferencesContext } from "../../context/preferences.context";
-
-import { useVendorData } from "../../context/vendor.context";
-import { useVendorIcon } from "../../hooks/useVendorIcon";
+import { useVendorData } from '../../context/vendor.context';
+import { useVendorIcon } from '../../hooks/useVendorIcon';
+import { useAuth } from '../../context/authorization.context';
 
 type Props = DrawerContentComponentProps<DrawerContentOptions>;
 
 export function DrawerContent(props: Props) {
   const { vendor } = useVendorData();
+  const { signOut, isSignedOut } = useAuth();
   const { vendorIcon, vendorInitials } = useVendorIcon();
+
   const paperTheme = useTheme();
-  const { rtl, theme, toggleRTL, toggleTheme } =
-    React.useContext(PreferencesContext);
+  const { rtl, theme, isThemeDark, toggleRTL, toggleTheme } =
+    usePreference();
 
   const translateX = Animated.interpolateNode(props.progress, {
     inputRange: [0, 0.5, 0.7, 0.8, 1],
     outputRange: [-100, -85, -70, -45, 0],
   });
+
+  console.log('Drawer: state', props.state);
+  console.log('Drawer: state routes', props.state.routes);
+  console.log('Drawer: state routenames', props.state.routeNames);
+  console.log('Drawer: navigation', props.navigation);
+
+  function getCurrentRouteName() {
+    let routes = props.state.routeNames;
+
+    console.log('DrawerContent: routes', routes);
+    // let tempRoute =
+    //   getFocusedRouteNameFromRoute(props.navigation.isFocused) ?? 'Store';
+    let route = routes[routes.length - 1];
+    console.log('DrawerContent: route', route);
+    if (route.length > 0) {
+      console.log(
+        'DrawerContent: route.length > 0',
+        route.length > 0
+      );
+      console.log(
+        'DrawerContent: return rout[route.length - 1]',
+        route[route.length - 1]
+      );
+      return route[route.length - 1];
+    }
+    return null;
+  }
+
+  const navigationWithPush = (
+    routeName: string,
+    params?: object | undefined
+  ) => {
+    let currentRoute;
+
+    /**
+     **  get route of current screen from the navigation props
+     **/
+    if (
+      Object.keys(props.state).length > 0 &&
+      props.state.routes != undefined
+    ) {
+      console.log(
+        'DrawerContent: navigationWithPush: if props.state.length > 0'
+      );
+      currentRoute = getCurrentRouteName();
+      console.log(
+        'DrawerContent: navigationWithPush: currentRoute = getCurrentRouteName',
+        currentRoute
+      );
+      console.log(
+        'DrawerContent: navigationWithPush: value of getCurrentRouteName',
+        getCurrentRouteName()
+      );
+    }
+
+    /**
+     ** if current route is the routeName itself then just close the drawer, else
+     ** push routeName to navigation state by dispatching the StackActions
+     **/
+
+    if (currentRoute === routeName) {
+      console.log(
+        'DrawerContent: navigationWithPush: currentRoute === routeName'
+      );
+      console.log(
+        'DrawerContent: navigationWithPush: currentroute:',
+        currentRoute
+      );
+      console.log(
+        'DrawerContent: navigationWithPush: routeName',
+        routeName
+      );
+      props.navigation.dispatch(DrawerActions.closeDrawer());
+    } else {
+      console.log(
+        'DrawerContent: navigationWithPush: currentRoute is not equal to routeName'
+      );
+      console.log(
+        'DrawerContent: navigationWithPush: currentroute:',
+        currentRoute
+      );
+      console.log(
+        'DrawerContent: navigationWithPush: routeName',
+        routeName
+      );
+      const pushAction = StackActions.push(routeName, params);
+      props.navigation.closeDrawer();
+      props.navigation.dispatch(pushAction);
+    }
+  };
 
   return (
     <DrawerContentScrollView {...props}>
@@ -72,29 +177,23 @@ export function DrawerContent(props: Props) {
             ) : (
               <Avatar.Icon size={50} icon="account" />
             )}
-            {/* <Avatar.Image
-              source={{
-                uri: "https://pbs.twimg.com/profile_images/952545910990495744/b59hSXUd_400x400.jpg",
-              }}
-              size={50}
-            /> */}
           </TouchableOpacity>
           <Title style={styles.title}>
-            {vendor?.name ? vendor.name : "Dawid"}
+            {vendor?.name ? vendor.name : 'Dawid'}
           </Title>
           <Caption style={styles.caption}>
-            {vendor?.online ? "Online" : "Offline"}
+            {vendor?.online ? 'Online' : 'Offline'}
           </Caption>
           <View style={styles.row}>
             <View style={styles.section}>
               <Paragraph style={[styles.paragraph, styles.caption]}>
-                {vendor?.product_count || "failed to fetch"}
+                {vendor?.product_count || 'failed to fetch'}
               </Paragraph>
               <Caption style={styles.caption}>Products</Caption>
             </View>
             <View style={styles.section}>
               <Paragraph style={[styles.paragraph, styles.caption]}>
-                {vendor?.order_count || "failed to fetch"}
+                {vendor?.order_count || 'failed to fetch'}
               </Paragraph>
               <Caption style={styles.caption}>Transactions</Caption>
             </View>
@@ -104,31 +203,86 @@ export function DrawerContent(props: Props) {
           <DrawerItem
             icon={({ color, size }) => (
               <MaterialCommunityIcons
-                name="account-outline"
+                name={ICONS.MY_PRODUCTS_ICON}
                 color={color}
                 size={size}
               />
             )}
-            label="Profile"
-            onPress={() => {}}
-          />
-          <DrawerItem
-            icon={({ color, size }) => (
-              <MaterialCommunityIcons name="tune" color={color} size={size} />
-            )}
-            label="Preferences"
-            onPress={() => {}}
+            label="My Products"
+            onPress={() => {
+              console.log('Drawer: pushed My Products');
+
+              navigationWithPush('Products', {
+                screen: 'My Products',
+                params: {
+                  screen: 'My Products',
+                },
+              });
+            }}
           />
           <DrawerItem
             icon={({ color, size }) => (
               <MaterialCommunityIcons
-                name="bookmark-outline"
+                name={ICONS.TRANSACTIONS_ICON}
                 color={color}
                 size={size}
               />
             )}
-            label="Bookmarks"
-            onPress={() => {}}
+            label="Transactions"
+            onPress={() => {
+              console.log('Drawer: pushed Transactions');
+
+              navigationWithPush('Products', {
+                screen: 'Transactions',
+              });
+            }}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name={ICONS.FAVORITES_ICON}
+                color={color}
+                size={size}
+              />
+            )}
+            label="Favorites"
+            onPress={() => {
+              console.log('Drawer: pushed Favorites');
+              navigationWithPush('Products', {
+                screen: 'Favorites',
+              });
+            }}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name={ICONS.PRODUCTS_ICON}
+                color={color}
+                size={size}
+              />
+            )}
+            label="All Products"
+            onPress={() => {
+              console.log('Drawer: pushed All Products');
+
+              navigationWithPush('Products');
+            }}
+          />
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name={ICONS.ADD_PRODUCT_ICON}
+                color={color}
+                size={size}
+              />
+            )}
+            label="Add Product"
+            onPress={() => {
+              console.log('Drawer: pushed Favorites');
+              navigationWithPush('Products', {
+                screen: 'Add Product',
+              });
+            }}
           />
         </Drawer.Section>
         <Drawer.Section title="Preferences">
@@ -136,7 +290,7 @@ export function DrawerContent(props: Props) {
             <View style={styles.preference}>
               <Text>Dark Theme</Text>
               <View pointerEvents="none">
-                <Switch value={theme === "dark"} />
+                <Switch value={isThemeDark} />
               </View>
             </View>
           </TouchableRipple>
@@ -144,7 +298,17 @@ export function DrawerContent(props: Props) {
             <View style={styles.preference}>
               <Text>RTL</Text>
               <View pointerEvents="none">
-                <Switch value={rtl === "right"} />
+                <Switch value={rtl === 'right'} />
+              </View>
+            </View>
+          </TouchableRipple>
+        </Drawer.Section>
+        <Drawer.Section title="Account">
+          <TouchableRipple onPress={() => signOut()}>
+            <View style={styles.preference}>
+              <Text>Sign Out</Text>
+              <View pointerEvents="none">
+                <Switch value={isSignedOut === false} />
               </View>
             </View>
           </TouchableRipple>
@@ -163,7 +327,7 @@ const styles = StyleSheet.create({
   },
   title: {
     marginTop: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   caption: {
     fontSize: 14,
@@ -171,24 +335,24 @@ const styles = StyleSheet.create({
   },
   row: {
     marginTop: 20,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   section: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginRight: 15,
   },
   paragraph: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 3,
   },
   drawerSection: {
     marginTop: 15,
   },
   preference: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     paddingVertical: 12,
     paddingHorizontal: 16,
   },
