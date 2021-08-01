613f68fd671f4b4b320a0a260f57b6478152b8c5
israelias <israelias.js@gmail.com>
2021-08-02 03:11:54 +0800

    Add constants and actions to productdetail component

--- frontend/src/components/products/productDetail.tsx
+++ frontend/src/components/products/productDetail.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {
   Surface,
   Title,
@@ -10,79 +15,88 @@ import {
   Badge,
   Button,
   Text,
-} from "react-native-paper";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+} from 'react-native-paper';
+import { StackNavigationProp } from '@react-navigation/stack';
+import color from 'color';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { ProductStackNavigatorParamList } from "../../types";
-import { TimeAgo } from "../common/time";
-import { useVendorData } from "../../context/vendor.context";
-import { useVendorIcon } from "../../hooks/useVendorIcon";
-import { useImagePlaceholder } from "../../hooks/useImagePlaceholder";
-import { useAvatarPlaceholder } from "../../hooks/useAvatarPlaceholder";
-import { initialize } from "../../utils/initialize";
-
+import { ProductStackNavigatorParamList } from '../../types';
+import { TimeAgo } from '../common/time';
+import { useVendorData } from '../../context/vendor.context';
+import { useVendorIcon } from '../../hooks/useVendorIcon';
+import { useImagePlaceholder } from '../../hooks/useImagePlaceholder';
+import { useAvatarPlaceholder } from '../../hooks/useAvatarPlaceholder';
+import { initialize } from '../../utils/initialize';
+import * as ICONS from '../../constants/icons.constants';
 import {
   DEFAULT_PLACEHOLDER,
   DEFAULT_AVATAR,
-} from "../../constants/backend.constants";
-
-type Props = {} & ProductStackNavigatorParamList["ProductDetails"];
+} from '../../constants/backend.constants';
 
 export const randomColor = () => {
   const hex = Math.floor(Math.random() * 0xffffff);
   return `${hex.toString(16)}`;
 };
 
-export const ProductDetail = (props: Props) => {
-  const product = props;
+export const ProductDetail = ({
+  product,
+  navigation,
+  makeOffer,
+  makePurchase,
+  addToFaves,
+}: {
+  product: ProductStackNavigatorParamList['ProductDetails'];
+  navigation?: StackNavigationProp<ProductStackNavigatorParamList>;
+  makeOffer?: (id: string) => void;
+  makePurchase?: (id: string) => void;
+  addToFaves?: (id: string) => void;
+}) => {
   const theme = useTheme();
 
   const { vendorId, vendorFaves } = useVendorData();
 
   const { vendorIcon, vendorInitials } = useVendorIcon();
 
-  const { productImage, productThumbnail } = useImagePlaceholder(product.image);
+  const { productImage, productThumbnail } = useImagePlaceholder(
+    product.image
+  );
 
   const { avatarImage } = useAvatarPlaceholder(product.vendor.image);
 
   const otherVendorInitials = initialize(product.vendor.name);
 
   const inFavorites = vendorFaves.some(
-    (favorite: string) => favorite === product.slug
+    (favorite) => favorite.id === product.id
   );
 
-  // const contentColor = color(theme.colors.text).alpha(0.8).rgb().string();
+  const contentColor = color(theme.colors.text)
+    .alpha(0.8)
+    .rgb()
+    .string();
 
-  const contentColor = theme.colors.text;
+  const imageBorderColor = color(theme.colors.text)
+    .alpha(0.15)
+    .rgb()
+    .string();
 
-  // const imageBorderColor = color(theme.colors.text).alpha(0.15).rgb().string();
-
-  const imageBorderColor = theme.colors.text;
-
-  const iconColor = theme.colors.text;
-
-  const ran = randomColor();
-  const dom = randomColor();
-  const vendorImage = `https://via.placeholder.com/150/${ran}/${dom}`;
-
-  // image.image.thumbnail
-  const tempImage = `https://via.placeholder.com/150/${ran}/${dom}`;
-
-  // similar.image.thumbnail
-
-  // const vendorIcon = tempImage;
+  const iconColor = color(theme.colors.text)
+    .alpha(0.54)
+    .rgb()
+    .string();
 
   return (
     <Surface style={styles.container}>
       <View style={styles.topRow}>
         <Avatar.Image
           style={styles.avatar}
-          source={{ uri: vendorImage }}
+          source={{ uri: avatarImage }}
           size={60}
         />
         <View>
           <Title>{product.title}</Title>
-          <Caption style={styles.handle}>{product.vendor.name}</Caption>
+          <Caption style={styles.handle}>
+            {product.vendor.name}
+          </Caption>
         </View>
       </View>
       <Subheading style={[styles.content, { color: contentColor }]}>
@@ -104,9 +118,13 @@ export const ProductDetail = (props: Props) => {
       <Text style={{ marginBottom: 10 }}>{product.price}</Text>
       <Text style={{ marginBottom: 10 }}>{product.price}</Text>
       <Text style={{ marginBottom: 10 }}>
-        {product.is_available ? "Posted" : "Sold"}{" "}
+        {product.is_available ? 'Posted' : 'Sold'}{' '}
         <TimeAgo
-          date={product.is_available ? product.created_at : product.updated_at}
+          date={
+            product.is_available
+              ? product.created_at
+              : product.updated_at
+          }
         />
       </Text>
 
@@ -122,7 +140,11 @@ export const ProductDetail = (props: Props) => {
                   mode="text"
                   uppercase
                   color={theme.colors.primary}
-                  onPress={() => console.log("Pressed")}
+                  onPress={() =>
+                    makePurchase
+                      ? makePurchase(product.id.toString())
+                      : {}
+                  }
                 >
                   Purchase
                 </Button>
@@ -134,9 +156,22 @@ export const ProductDetail = (props: Props) => {
                 mode="text"
                 uppercase
                 color={theme.colors.primary}
-                onPress={() => console.log("Pressed")}
+                onPress={() => {
+                  // makeOffer ? makeOffer(product.id.toString()) : {}
+                  console.log('product', product);
+                  navigation &&
+                    navigation.replace('MakeOffer', {
+                      product: product,
+                      productId: product.id.toString(),
+                      vendorId: product.vendor.id.toString(),
+
+                      ...product,
+                      // productId: product.id.toString(),
+                      // sellerId: product.vendor.id
+                    });
+                }}
               >
-                Purchase
+                Offer
               </Button>
             </View>
           </>
@@ -149,7 +184,9 @@ export const ProductDetail = (props: Props) => {
           <View style={styles.bottomRow}>
             <View>
               <Title>Product Images</Title>
-              <Caption style={styles.handle}>{product.vendor.name}</Caption>
+              <Caption style={styles.handle}>
+                {product.vendor.name}
+              </Caption>
             </View>
 
             {product.product_images.map((image) => (
@@ -173,42 +210,52 @@ export const ProductDetail = (props: Props) => {
           </View>
         </>
       )}
-      {product.similar_products && product.similar_products.length > 0 && (
-        <>
-          <View style={{ height: StyleSheet.hairlineWidth }} />;
-          <Title>Related Products</Title>
-          <Caption style={styles.handle}>{product.category}</Caption>
-          {product.similar_products.map((similar) => (
-            <View style={styles.bottomRow}>
-              <Title>{similar.title}</Title>
-              {similar.id.toString() === vendorId ? (
-                <Caption style={styles.handle}>Your Product</Caption>
-              ) : (
-                <Caption style={styles.handle}>{similar.vendor}</Caption>
-              )}
+      {product.similar_products &&
+        product.similar_products.length > 0 && (
+          <>
+            <View style={{ height: StyleSheet.hairlineWidth }} />;
+            <Title>Related Products</Title>
+            <Caption style={styles.handle}>
+              {product.category}
+            </Caption>
+            {product.similar_products.map((similar) => (
+              <View style={styles.bottomRow}>
+                <Title>{similar.title}</Title>
+                {similar.id.toString() === vendorId ? (
+                  <Caption style={styles.handle}>
+                    Your Product
+                  </Caption>
+                ) : (
+                  <Caption style={styles.handle}>
+                    {similar.vendor}
+                  </Caption>
+                )}
 
-              <TouchableOpacity
-                onPress={() => {}}
-                hitSlop={{ top: 10, bottom: 10 }}
-              >
-                <Avatar.Image
-                  style={{ marginRight: 10 }}
-                  source={{
-                    uri: similar.image.thumbnail
-                      ? similar.image.thumbnail
-                      : similar.image.full_size
-                      ? similar.image.full_size
-                      : DEFAULT_PLACEHOLDER,
-                  }}
-                  size={40}
-                />
-              </TouchableOpacity>
-            </View>
-          ))}
-        </>
-      )}
+                <TouchableOpacity
+                  onPress={() => {}}
+                  hitSlop={{ top: 10, bottom: 10 }}
+                >
+                  <Avatar.Image
+                    style={{ marginRight: 10 }}
+                    source={{
+                      uri: similar.image.thumbnail
+                        ? similar.image.thumbnail
+                        : similar.image.full_size
+                        ? similar.image.full_size
+                        : DEFAULT_PLACEHOLDER,
+                    }}
+                    size={40}
+                  />
+                </TouchableOpacity>
+              </View>
+            ))}
+          </>
+        )}
       <View style={styles.footerRow}>
-        <TouchableOpacity style={{ marginLeft: 10 }} onPress={() => {}}>
+        <TouchableOpacity
+          style={{ marginLeft: 10 }}
+          onPress={() => {}}
+        >
           <View>
             {product.id.toString() === vendorId ? (
               vendorIcon ? (
@@ -250,7 +297,7 @@ export const ProductDetail = (props: Props) => {
 
             <Badge
               style={{
-                position: "absolute",
+                position: 'absolute',
                 bottom: 0,
                 right: 4,
                 backgroundColor: product.vendor.online
@@ -264,26 +311,26 @@ export const ProductDetail = (props: Props) => {
         {product.vendor.product_count > 0 && (
           <View style={styles.iconContainer}>
             <MaterialCommunityIcons
-              name="package-variant-closed"
+              name={ICONS.PRODUCT_COUNT_ICON}
               size={14}
               color={iconColor}
             />
             <Caption style={styles.iconDescription}>
               {product.vendor.product_count}
-              {""} products
+              {''} products
             </Caption>
           </View>
         )}
         {product.vendor.order_count > 0 && (
           <View style={styles.iconContainer}>
             <MaterialCommunityIcons
-              name="currency-usd-off"
+              name={ICONS.ORDER_COUNT_ICON}
               size={14}
               color={iconColor}
             />
             <Caption style={styles.iconDescription}>
               {product.vendor.order_count}
-              {""} transactions
+              {''} transactions
             </Caption>
           </View>
         )}
@@ -301,8 +348,8 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   topRow: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   handle: {
     marginRight: 3,
@@ -317,29 +364,29 @@ const styles = StyleSheet.create({
     borderWidth: StyleSheet.hairlineWidth,
     marginTop: 25,
     borderRadius: 20,
-    width: "100%",
+    width: '100%',
     height: 280,
   },
   thumbnail: {
     borderWidth: StyleSheet.hairlineWidth,
     marginTop: 25,
     borderRadius: 10,
-    width: "25%",
+    width: '25%',
     height: 140,
   },
   bottomRow: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 10,
   },
   footerRow: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     marginBottom: 10,
   },
   iconContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   iconDescription: {
     marginLeft: 2,
