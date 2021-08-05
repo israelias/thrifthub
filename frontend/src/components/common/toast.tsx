4b494556b3662fca30505b00faad593fff828196
israelias <israelias.js@gmail.com>
2021-08-05 23:20:04 +0800

    Update toast snackbar styles

--- frontend/src/components/common/toast.tsx
+++ frontend/src/components/common/toast.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { View, StyleSheet } from "react-native";
-import { Button, Snackbar } from "react-native-paper";
+import React from 'react';
+import color from 'color';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { View, StyleSheet } from 'react-native';
+import {
+  Button,
+  Snackbar,
+  useTheme,
+  Avatar,
+  Text,
+} from 'react-native-paper';
+import overlay from '../../utils/overlay';
 
 /**
  * The main prompt for all messages following fetch methods.
@@ -18,7 +27,7 @@ export const Toast = ({
   warning,
   info,
   message,
-  duration = 750,
+  duration = 2500,
 }: {
   success?: boolean;
   error?: boolean;
@@ -30,24 +39,74 @@ export const Toast = ({
   label?: string;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const theme = useTheme();
+  const safeArea = useSafeAreaInsets();
+
+  const surfaceColor = theme.dark
+    ? (overlay(6, theme.colors.surface) as string)
+    : theme.colors.background;
+
+  const textColor = color(theme.colors.text)
+    .alpha(0.6)
+    .rgb()
+    .string();
+
+  const contentColor = color(theme.colors.text)
+    .alpha(0.8)
+    .rgb()
+    .string();
+
+  const borderColor = color(theme.colors.text)
+    .alpha(0.15)
+    .rgb()
+    .string();
+
   return (
     <View style={styles.container}>
       <Snackbar
+        color={textColor}
+        style={[
+          styles.snackBar,
+          {
+            bottom: safeArea.bottom + 65,
+            backgroundColor: surfaceColor,
+            borderColor: borderColor,
+          },
+        ]}
         visible={open}
         onDismiss={() => setOpen(false)}
         duration={duration}
         action={{
-          label: error
-            ? "Error"
-            : warning
-            ? "Warning"
-            : info
-            ? "Info"
-            : "Success",
+          // @ts-ignore
+          label: error ? (
+            <Avatar.Icon
+              size={24}
+              icon="alert-decagram-outline"
+              color={theme.colors.error}
+            />
+          ) : warning ? (
+            <Avatar.Icon
+              size={24}
+              icon="alert-outline"
+              color={theme.colors.error}
+            />
+          ) : info ? (
+            <Avatar.Icon
+              size={24}
+              icon="information-outline"
+              color={theme.colors.notification}
+            />
+          ) : (
+            <Avatar.Icon
+              size={24}
+              icon="check"
+              color={theme.colors.accent}
+            />
+          ),
           onPress: () => setOpen(false),
         }}
       >
-        {message}
+        <Text style={{ color: contentColor }}>{message}</Text>
       </Snackbar>
     </View>
   );
@@ -56,6 +115,9 @@ export const Toast = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
+  },
+  snackBar: {
+    borderWidth: StyleSheet.hairlineWidth,
   },
 });
