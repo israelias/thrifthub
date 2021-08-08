142998f71af2d2f8e53ef1b75d4d8c7c4820afb5
israelias <israelias.js@gmail.com>
2021-08-08 22:46:09 +0800

    Update messages component

--- frontend/src/components/account/messages.tsx
+++ frontend/src/components/account/messages.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import { MaterialBottomTabNavigationProp } from "@react-navigation/material-bottom-tabs";
-import { Headline, Caption, useTheme, Button } from "react-native-paper";
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { MaterialBottomTabNavigationProp } from '@react-navigation/material-bottom-tabs';
+import {
+  Headline,
+  Caption,
+  useTheme,
+  Button,
+} from 'react-native-paper';
 
-// import overlay from './overlay';
+import overlay from '../../utils/overlay';
 
 type Props = {
   navigation: MaterialBottomTabNavigationProp<{}>;
@@ -12,26 +17,28 @@ type Props = {
 export const Message = () => {
   const theme = useTheme();
 
-  // const backgroundColor = overlay(2, theme.colors.surface) as string;
-  const backgroundColor = theme.colors.surface;
+  const backgroundColor = overlay(2, theme.colors.surface) as string;
 
   return (
     <ScrollView
       style={{ backgroundColor }}
-      contentContainerStyle={[styles.scrollViewContent, { backgroundColor }]}
+      contentContainerStyle={[
+        styles.scrollViewContent,
+        { backgroundColor },
+      ]}
     >
       <Headline style={styles.centerText}>
         Send a message, get a message
       </Headline>
       <Caption style={styles.centerText}>
-        Private Messages are private conversations between you and other people
-        on Twitter. Share Tweets, media, and more!
+        Private Messages are private conversations between you and
+        other people on Twitter. Share Tweets, media, and more!
       </Caption>
       <Button
         onPress={() => {}}
         style={styles.button}
         mode="contained"
-        labelStyle={{ color: "white" }}
+        labelStyle={{ color: 'white' }}
       >
         Write a message
       </Button>
@@ -43,11 +50,11 @@ const styles = StyleSheet.create({
   scrollViewContent: {
     flex: 1,
     paddingHorizontal: 30,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   centerText: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   button: {
     marginTop: 20,
