add25a221c568a4ba1f87fab39a52a01ffd4ba06
israelias <israelias.js@gmail.com>
2021-08-08 22:39:30 +0800

    Rewrite core.tsx to respond to state variables

--- frontend/src/core.tsx
+++ frontend/src/core.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-import { Platform, ActivityIndicator } from 'react-native';
+import { Platform, ActivityIndicator, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  PathConfigMap,
+  InitialState,
+} from '@react-navigation/native';
 import * as Linking from 'expo-linking';
 
 import { StatusBar } from 'expo-status-bar';
@@ -17,19 +21,72 @@ import { usePreference } from './context/preferences.context';
 import useCachedResources from './hooks/useCachedResources';
 import UserProvider from './context/user.context';
 
+import { NAVIGATION_PERSISTENCE_KEY } from './constants/keys.constants';
+
 import { Splash } from './components/common/splash';
 
-const PERSISTENCE_KEY = 'NAVIGATION_STATE';
+import { ProductTypeParamList, RootStackParamList } from './types';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+const SCREENS = {
+  StartScreen: { title: 'Native Stack', component: {} },
+  LoginScreen: { title: 'Simple Stack', component: {} },
+  RegisterScreen: {
+    title: 'Modal Stack',
+    component: {},
+  },
+  Products: {
+    title: 'Regular + Modal Stack',
+    component: {},
+  },
+  ProductDetails: {
+    title: 'Float + Screen Header Stack',
+    component: {},
+  },
+  MakeOffer: {
+    title: 'Transparent Stack',
+    component: {},
+  },
+  MakePurchase: {
+    title: 'Header Customization in Stack',
+    component: {},
+  },
+  UpdateProduct: {
+    title: 'Header Customization in Native Stack',
+    component: {},
+  },
+  Transactions: { title: 'Bottom Tabs', component: {} },
+  Favorites: {
+    title: 'Material Top Tabs',
+    component: {},
+  },
+  MyProducts: {
+    title: 'Material Bottom Tabs',
+    component: {},
+  },
+  AddProduct: {
+    title: 'Dynamic Tabs',
+    component: {},
+  },
+};
 
 export const Core = () => {
-  const { isThemeDark, theme } = usePreference();
+  const { theme } = usePreference();
 
   const isLoadingComplete = useCachedResources();
 
   const [isReady, setIsReady] = React.useState(
     __DEV__ ? false : true
   );
-  const [initialState, setInitialState] = React.useState();
+  const [initialState, setInitialState] = React.useState<
+    InitialState | undefined
+  >();
 
   const [showSplash, setShowSplash] = React.useState(true);
 
@@ -41,7 +98,7 @@ export const Core = () => {
         if (Platform.OS !== 'web' && initialUrl == null) {
           // Only restore state if there's no deep link and we're not on web
           const savedStateString = await AsyncStorage.getItem(
-            PERSISTENCE_KEY
+            NAVIGATION_PERSISTENCE_KEY
           );
           const state = savedStateString
             ? JSON.parse(savedStateString)
@@ -76,109 +133,156 @@ export const Core = () => {
     return <Splash isVisible={!isReady || !isLoadingComplete} />;
   }
 
-  // let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
-
-  // const onReady = ((
-  //   isReadyRef as React.MutableRefObject<boolean>
-  // ).current = true);
-
-  // console.log('onReady', onReady);
-
-  // React.useEffect(() => {
-  //   return () => {
-  //     (isReadyRef as React.MutableRefObject<boolean>).current = false;
-  //   };
-  // }, []);
-
-  // React.useEffect(() => {
-  //   console.log(
-  //     'NavigationContainerRef: getRootState',
-  //     navigationRef?.current?.getRootState()
-  //   );
-  //   console.log(
-  //     'NavigationContainerRef: getCurrentRoute',
-  //     navigationRef?.current?.getCurrentRoute()
-  //   );
-  //   console.log(
-  //     'NavigationContainerRef: getCurrentOptions',
-  //     navigationRef?.current?.getCurrentOptions()
-  //   );
-  // }, [navigationRef]);
-
   console.log('Core: Linking.getInitialUrl', Linking.getInitialURL());
 
   return (
-    <NavigationContainer
-      initialState={initialState}
-      onStateChange={(state) => {
-        console.log(
-          'NavigationContainer: onStateChange',
-          JSON.stringify(state)
-        );
-        AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state));
-      }}
-      documentTitle={{
-        formatter: (options, route) =>
-          `${options?.title ?? route?.name} - ThriftHub`,
-      }}
-      theme={theme}
-      linking={{
-        prefixes: [Linking.makeUrl('/')],
-        enabled: true,
-        config: {
-          screens: {
-            Store: {
-              screens: {
-                Products: {
+    <PaperProvider theme={theme}>
+      <StatusBar
+        translucent
+        animated={true}
+        style={theme.dark ? 'light' : 'dark'}
+        backgroundColor="rgba(0, 0, 0, 0.24)"
+      />
+
+      <NavigationContainer
+        initialState={initialState}
+        onStateChange={(state) => {
+          console.log(
+            'NavigationContainer: onStateChange',
+            JSON.stringify(state)
+          );
+          AsyncStorage.setItem(
+            NAVIGATION_PERSISTENCE_KEY,
+            JSON.stringify(state)
+          );
+        }}
+        theme={theme}
+        linking={{
+          // To test deep linking on, run the following in the Terminal:
+          // Android: adb shell am start -a android.intent.action.VIEW -d "exp://127.0.0.1:19000/--/simple-stack"
+          // iOS: xcrun simctl openurl booted exp://127.0.0.1:19000/--/simple-stack
+          // Android (bare): adb shell am start -a android.intent.action.VIEW -d "rne://127.0.0.1:19000/--/simple-stack"
+          // iOS (bare): xcrun simctl openurl booted rne://127.0.0.1:19000/--/simple-stack
+          // The first segment of the link is the the scheme + host (returned by `Linking.makeUrl`)
+          prefixes: [Linking.createURL('/')],
+          enabled: true,
+          config: {
+            initialRouteName: 'Home',
+            screens: Object.keys(SCREENS).reduce<
+              PathConfigMap<RootStackParamList>
+            >(
+              (acc, name) => {
+                // Convert screen names such as SimpleStack to kebab case (simple-stack)
+                const path = name
+                  .replace(/([A-Z]+)/g, '-$1')
+                  .replace(/^-/, '')
+                  .toLowerCase();
+
+                console.log('CONFIG path', path);
+                console.log('CONFIG namge', name);
+                // @ts-expect-error: these types aren't accurate
+                // But we aren't too concerned for now
+                acc[name] = {
+                  path,
                   screens: {
                     ProductScreen: 'products',
                     ProductDetailScreen: {
-                      path: 'products/:slug',
+                      path: 'products/:slug?',
                       parse: {
-                        slug: String,
+                        slug: (slug: string) =>
+                          slug.charAt(0).toUpperCase() +
+                          slug.slice(1).replace(/-/g, ' '),
+                      },
+                      stringify: {
+                        slug: (slug: string) =>
+                          slug.toLowerCase().replace(/\s/g, '-'),
                       },
                     },
+                    MakeOfferScreen: {
+                      path: `${path}/offer/:product`,
+                      parse: {
+                        product: (product: ProductTypeParamList) =>
+                          product.slug.charAt(0).toUpperCase() +
+                          product.slug.slice(1).replace(/-/g, ' '),
+                      },
+                      stringify: {
+                        product: (product: ProductTypeParamList) =>
+                          product.slug
+                            .toLowerCase()
+                            .replace(/\s/g, '-'),
+                      },
+                    },
+                    MakePurchaseScreen: {
+                      path: 'products/purchase/:slug',
+                      parse: {
+                        slug: (slug: string) =>
+                          slug.charAt(0).toUpperCase() +
+                          slug.slice(1).replace(/-/g, ' '),
+                      },
+                      stringify: {
+                        slug: (slug: string) =>
+                          slug.toLowerCase().replace(/\s/g, '-'),
+                      },
+                    },
+                    UpdateProductScreen: {
+                      path: 'products/update/:slug?',
+                      parse: {
+                        slug: (slug: string) =>
+                          slug.charAt(0).toUpperCase() +
+                          slug.slice(1).replace(/-/g, ' '),
+                      },
+                      stringify: {
+                        slug: (slug: string) =>
+                          slug.toLowerCase().replace(/\s/g, '-'),
+                      },
+                    },
+                    TransactionOrdersScreen: {
+                      screens: {
+                        orderRequests: {
+                          path: 'transactions/sales',
+                        },
+                        ordersMade: {
+                          path: 'transactions/purchases',
+                        },
+                      },
+                    },
+                    FavoriteProductsScreen: 'favorites',
+                    MyProductsScreen: 'myproducts',
+                    AddProductScreen: 'add',
                   },
-                },
-                Transactions: {
-                  screens: {
-                    TransactionOrdersScreen: 'two',
-                  },
-                },
-                // Products: {
-                //   path: 'products/:slug',
-                //   parse: {
-                //     slug: String,
-                //   },
-                // },
+                };
+
+                return acc;
               },
-            },
-            NotFound: '*',
+              {
+                Home: {
+                  screens: {
+                    StartScreen: '',
+                    LoginScreen: 'signin',
+                    RegisterScreen: 'signup',
+                  },
+                },
+                NotFound: '*',
+              }
+            ),
           },
-        },
-      }}
-    >
-      <>
-        <PaperProvider theme={theme}>
-          <StatusBar
-            animated={true}
-            style={isThemeDark ? 'light' : 'dark'}
-          />
-          <ProductsDataProvider>
-            <VendorDataProvider>
-              <AuthorizationProvider>
-                <>
-                  <>
-                    <UserProvider>
-                      <CoreNavigator />
-                    </UserProvider>
-                  </>
-                </>
-              </AuthorizationProvider>
-            </VendorDataProvider>
-          </ProductsDataProvider>
-        </PaperProvider>
-      </>
-    </NavigationContainer>
+        }}
+        fallback={<Text>Loading…</Text>}
+        documentTitle={{
+          formatter: (options, route) =>
+            `${options?.title ?? route?.name} - ThriftHub`,
+        }}
+      >
+        <ProductsDataProvider>
+          <VendorDataProvider>
+            <AuthorizationProvider>
+              <UserProvider>
+                <CoreNavigator />
+              </UserProvider>
+            </AuthorizationProvider>
+          </VendorDataProvider>
+        </ProductsDataProvider>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
