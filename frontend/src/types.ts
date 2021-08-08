4467b7f69affb4d76ed752f7cbb24c6604632d07
israelias <israelias.js@gmail.com>
2021-08-08 22:38:51 +0800

    Update param types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,83 +1,86 @@
-import { StackNavigationProp } from "@react-navigation/stack";
-import { RouteProp } from "@react-navigation/native";
-import { GestureResponderEvent } from "react-native";
+import React from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
+import {
+  RouteProp,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
+// import { GestureResponderEvent } from "react-native";
 
-export type ProductStackNavigatorParamList = {
-  Products: undefined;
-  ProductDetails: {
-    id: number;
-    category: {
-      id: number;
-      name: string;
-      slug: string;
-    };
-    // vendor: {
-    //   id: number;
-    //   name: string;
-    //   online: boolean;
-    //   image: {
-    //     full_size: string;
-    //     thumbnail: string;
-    //   };
-    //   product_count: number;
-    //   order_count: number;
-    //   favorites: {
-    //     id: number;
-    //     category: string;
-    //     vendor: string;
-    //     title: string;
-    //     slug: string;
-    //     price: number;
-    //     condition: string;
-    //     is_available: boolean;
-    //     image: {
-    //       full_size: string;
-    //       thumbnail: string;
-    //     };
-    //   }[];
-    // };
-    vendor: VendorTypeParamList;
-    title: string;
-    description: string;
-    slug: string;
-    price: number;
-    condition: string;
-    is_available: boolean;
-    created_at: string;
-    updated_at: string;
-    image: {
-      full_size: string;
-      thumbnail: string;
-    };
-    product_images: {
-      image: {
-        full_size: string;
-        thumbnail: string;
-      };
-      // alt_text: string;
-      // is_feature: boolean;
-    }[];
-    similar_products: {
-      id: number;
-      category: string;
-      vendor: string;
-      title: string;
-      slug: string;
-      price: number;
-      condition: string;
-      is_available: boolean;
-      image: {
-        full_size: string;
-        thumbnail: string;
-      };
-    }[];
-  };
+// export type ProductParamList = {
+//   Product: {
+//     name: string;
+//   };
+//   ProductDetail: {
+//     name: string;
+//     submit?: React.MutableRefObject<() => void>;
+//   };
+// };
+
+// export type AuthParamList = {
+//   Login: undefined;
+//   Register: undefined;
+// };
+
+// export type AuthNavProps<T extends keyof AuthParamList> = {
+//   navigation: StackNavigationProp<AuthParamList, T>;
+//   route: RouteProp<AuthParamList, T>;
+// };
+
+// export type HomeParamList = {
+//   Landing: AuthParamList;
+// } & ProductParamList;
+
+// export type HomeStackNavProps<T extends keyof HomeParamList> = {
+//   navigation: StackNavigationProp<HomeParamList, T>;
+//   route: RouteProp<HomeParamList, T>;
+// };
+
+/**
+ * Root Stack
+ */
+export type RootStackParamList = {
+  Home: NavigatorScreenParams<AccountStackNavigatorParamList>;
+  NotFound: undefined;
+} & AccountStackNavigatorParamList &
+  ProductStackNavigatorParamList;
+
+/**
+ * Auth Stack
+ */
+export type AccountStackNavigatorParamList = {
+  StartScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
 };
 
-type CategoryTypeParamList = {
-  id: number;
-  name: string;
-  slug: string;
+export type DrawerStackParamList = {
+  Home: undefined;
+} & BottomTabParamList &
+  ProductStackNavigatorParamList;
+
+export type BottomTabParamList = {
+  Products: undefined;
+  Transactions: undefined;
+  Favorites: undefined;
+  MyProducts: undefined;
+  AddProduct: undefined;
+};
+
+/**
+ * Products Home Stack
+ */
+export type ProductStackNavigatorParamList = {
+  Products: undefined;
+  ProductDetails: ProductTypeParamList;
+  MakeOffer: {
+    product: ProductTypeParamList;
+  };
+  MakePurchase: {
+    product: ProductTypeParamList;
+  };
+  UpdateProduct: {
+    product: ProductTypeParamList;
+  };
 };
 
 export type ProductTypeParamList = {
@@ -90,42 +93,66 @@ export type ProductTypeParamList = {
   price: number;
   condition: string;
   is_available: boolean;
-  image: {
-    full_size: string;
-    thumbnail: string;
-  };
+  image: ImageTypeParamList;
   created_at: string;
   updated_at: string;
-  product_images: {
-    image: {
-      full_size: string;
-      thumbnail: string;
-    };
-  }[];
-  similar_products: {
-    id: number;
-    category: string;
-    vendor: string;
-    title: string;
-    slug: string;
-    price: number;
-    condition: string;
-    is_available: boolean;
-    image: {
-      full_size: string;
-      thumbnail: string;
-    };
-  }[];
+  product_images: ProductImagesTypeParamList;
+  similar_products: SimilarProductsTypeParamList;
 };
+
+export type AccountStackNavProps<
+  T extends keyof AccountStackNavigatorParamList
+> = {
+  navigation: StackNavigationProp<AccountStackNavigatorParamList, T>;
+  route: RouteProp<AccountStackNavigatorParamList, T>;
+};
+
+export type RootStackNavProps<T extends keyof RootStackParamList> = {
+  navigation: StackNavigationProp<RootStackParamList, T>;
+  route: RouteProp<RootStackParamList, T>;
+};
+
+export type SearchParamList = {
+  Search: undefined;
+} & ProductStackNavigatorParamList;
+
+export type SearchStackNavProps<T extends keyof SearchParamList> = {
+  navigation: StackNavigationProp<SearchParamList, T>;
+  route: RouteProp<SearchParamList, T>;
+};
+
+export type CategoryTypeParamList = {
+  id: number;
+  name: string;
+  slug: string;
+};
+
+export type ProductImagesTypeParamList = Array<{
+  image: ImageTypeParamList;
+}>;
+
+export type ImageTypeParamList = {
+  full_size: string;
+  thumbnail: string;
+};
+
+export type SimilarProductsTypeParamList = Array<{
+  id: number;
+  category: string;
+  vendor: string;
+  title: string;
+  slug: string;
+  price: number;
+  condition: string;
+  is_available: boolean;
+  image: ImageTypeParamList;
+}>;
 
 export type VendorTypeParamList = {
   id: number;
   name: string;
   online: boolean;
-  image: {
-    full_size: string;
-    thumbnail: string;
-  };
+  image: ImageTypeParamList;
   products: ProductTypeParamList[];
   product_count: number;
   order_count: number;
@@ -155,19 +182,7 @@ export type OrderTransactionStackParamList =
     }
   | Record<string, object | undefined>;
 
-type Order = {
-  id: number;
-  product: ProductPreview;
-  vendor: VendorPreview;
-  buyer: VendorPreview;
-  status: string;
-  amount?: string;
-  created_at?: string;
-  updated_at?: string;
-  order_detail?: OrderDetailTypeParamList;
-};
-
-type OrderDetailTypeParamList = {
+export type OrderDetailTypeParamList = {
   id: number;
   full_name: string;
   email: string;
@@ -189,14 +204,6 @@ export type AuthResponseType = {
   user: VendorTypeParamList;
 };
 
-export type AccountStackNavigatorParamList = {
-  StartScreen: undefined;
-  LoginScreen: undefined;
-  RegisterScreen: undefined;
-  Dashboard: undefined;
-  ResetPassword: undefined;
-};
-
 export type AuthStackNavigationParamList = {
   SplashScreen: undefined;
   SignInScreen: undefined;
@@ -208,33 +215,4 @@ export type FullStackParamLost = {
   Account: AccountStackNavigatorParamList;
   Store: ProductStackNavigatorParamList;
   // Account:
-};
-
-export type ProductParamList = {
-  Product: {
-    name: string;
-  };
-  ProductDetail: {
-    name: string;
-    submit?: React.MutableRefObject<() => void>;
-  };
-};
-
-export type AuthParamList = {
-  Login: undefined;
-  Register: undefined;
-};
-
-export type AuthNavProps<T extends keyof AuthParamList> = {
-  navigation: StackNavigationProp<AuthParamList, T>;
-  route: RouteProp<AuthParamList, T>;
-};
-
-export type HomeParamList = {
-  Landing: AuthParamList;
-} & ProductParamList;
-
-export type HomeStackNavProps<T extends keyof HomeParamList> = {
-  navigation: StackNavigationProp<HomeParamList, T>;
-  route: RouteProp<HomeParamList, T>;
 };
