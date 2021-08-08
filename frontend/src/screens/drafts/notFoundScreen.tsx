39e0d4a28e164b7bc1f47a05268813ef73164f73
israelias <israelias.js@gmail.com>
2021-08-08 22:55:03 +0800

    Update export in notfoundscreen

--- frontend/src/screens/drafts/notFoundScreen.tsx
+++ frontend/src/screens/drafts/notFoundScreen.tsx
@@ -1,16 +1,21 @@
-import { StackScreenProps } from "@react-navigation/stack";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StackScreenProps } from '@react-navigation/stack';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-import { RootStackParamList } from "../../../types";
+import { RootStackParamList } from '../../../types';
 
-export default function NotFoundScreen({
+export function NotFoundScreen({
   navigation,
-}: StackScreenProps<RootStackParamList, "NotFound">) {
+}: StackScreenProps<RootStackParamList, 'NotFound'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
       <TouchableOpacity
-        onPress={() => navigation.replace("Root")}
+        onPress={() => navigation.replace('Root')}
         style={styles.link}
       >
         <Text style={styles.linkText}>Go to home screen!</Text>
@@ -22,14 +27,14 @@ export default function NotFoundScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 20,
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   link: {
     marginTop: 15,
@@ -37,6 +42,6 @@ const styles = StyleSheet.create({
   },
   linkText: {
     fontSize: 14,
-    color: "#2e78b7",
+    color: '#2e78b7',
   },
 });
