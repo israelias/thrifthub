68a801774953b35d3447fd6b4ee3824ae6b2c62d
Joem Elias Sanez <61286823+israelias@users.noreply.github.com>
2023-09-01 20:22:03 +0800

    Attach region to S3 endpoint

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -189,7 +189,9 @@ if "USE_AWS" in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = (
+        f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+    )
 
     # Static and media files
     STATICFILES_STORAGE = "core.storages.StaticStorage"
